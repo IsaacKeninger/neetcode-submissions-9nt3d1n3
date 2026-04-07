249fed8a4de3c99d125e87b6f436dee95286be2e
Isaac Keninger <isaac.keninger@gmail.com>
2026-04-07 15:10:18 -0500

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        l,r = 0, 1
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                curr = prices[r] - prices[l]
+                max_profit = max(curr, max_profit)
+            else:
+                l = r
+            r += 1
+
+        return max_profit
+        
