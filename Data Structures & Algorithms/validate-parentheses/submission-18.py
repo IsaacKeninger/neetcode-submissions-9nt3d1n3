d621e95c3d82fa9696b8eb01d7cc6a43781f8db2
Isaac Keninger <isaac.keninger@gmail.com>
2026-04-21 16:06:48 -0500

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        r_map = {')':'(', ']':'[', '}':'{'}
+
+        for c in s:
+            if c in r_map:
+                if stack and r_map[c] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return stack == []
