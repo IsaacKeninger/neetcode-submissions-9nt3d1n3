f8f8f362768a0bcda7dad5f13f90c7d9c3c36b7e
Isaac Keninger <isaac.keninger@gmail.com>
2026-04-29 10:55:51 -0500

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = high - (high - low) // 2 # calculate midpoint
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+        
+        return -1 # target not found
+
+            
+
+
+        
